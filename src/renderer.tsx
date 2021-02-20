ed724ac4113e58eecf234557cb971de6cac3f4f8
Yago Ernandes <yago700@gmail.com>
2021-02-20 00:08:55 -0300

    First commit

---
+++ src/renderer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/app';
+console.log('======================================')
+ReactDOM.render(<App />, document.getElementById('root'));
